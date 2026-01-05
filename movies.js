667514c119d00b64b6674b01da94d50f1521e551
danielmoraes9776-prog <danielmoraes9776@gmail.com>
2026-01-05 09:41:24 -0300

    Add files via upload

--- movies.js
+++ movies.js
@@ -1,17 +1,50 @@
 const movies = [
   {
     title: "Night of the Living Dead (1968)",
-    poster: "https://archive.org/download/night_of_the_living_dead/night_of_the_living_dead.jpg",
+    poster: "https://archive.org/services/img/night_of_the_living_dead",
     video: "https://archive.org/embed/night_of_the_living_dead"
   },
   {
-    title: "The Phantom of the Opera (1925)",
-    poster: "https://archive.org/download/phantom_of_the_opera/phantom.jpg",
-    video: "https://archive.org/embed/phantom_of_the_opera"
+    title: "Plan 9 from Outer Space (1959)",
+    poster: "https://archive.org/services/img/Plan9fromOuterSpace",
+    video: "https://archive.org/embed/Plan9fromOuterSpace"
   },
   {
-    title: "Sherlock Holmes (1916)",
-    poster: "https://archive.org/download/sherlock_holmes_1916/sherlock.jpg",
-    video: "https://archive.org/embed/sherlock_holmes_1916"
-  }
-];
+    title: "Charade (1963)",
+    poster: "https://archive.org/services/img/Charade1963",
+    video: "https://archive.org/embed/Charade1963"
+  },
+  {
+    title: "The General (1926)",
+    poster: "https://archive.org/services/img/TheGeneral1926",
+    video: "https://archive.org/embed/TheGeneral1926"
+  },
+  {
+    title: "Metropolis (1927)",
+    poster: "https://archive.org/services/img/metropolis_1927",
+    video: "https://archive.org/embed/metropolis_1927"
+  },
+  {
+    title: "Nosferatu (1922)",
+    poster: "https://archive.org/services/img/nosferatu_1922",
+    video: "https://archive.org/embed/nosferatu_1922"
+  },
+  {
+    title: "Flash Gordon – Ep. 1",
+    poster: "https://archive.org/services/img/flash_gordon_serial",
+    video: "https://archive.org/embed/flash_gordon_serial"
+  },
+  {
+    title: "Popeye Classic",
+    poster: "https://archive.org/services/img/popeye_classic",
+    video: "https://archive.org/embed/popeye_classic"
+  },
+  {
+    title: "Betty Boop Collection",
+    poster: "https://archive.org/services/img/betty_boop",
+    video: "https://archive.org/embed/betty_boop"
+  },
+  {
+    title: "Superman (1940s)",
+    poster: "https://archive.org/services/img/superman_1940s",
+    video:
