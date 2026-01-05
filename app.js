6a49859402eebac50345f9d3368a5e70a247d1e3
danielmoraes9776-prog <danielmoraes9776@gmail.com>
2026-01-05 10:00:22 -0300

    Update app.js

--- app.js
+++ app.js
@@ -5,19 +5,20 @@ document.addEventListener("DOMContentLoaded", () => {
     const card = document.createElement("div");
     card.className = "movie";
 
-    const img = document.createElement("img");
-    img.src = movie.poster;
-    img.alt = movie.title;
+    const poster = document.createElement("div");
+    poster.className = "poster";
+    poster.style.backgroundImage = `url(${movie.poster})`;
 
-    // 👉 CAPA RESERVA (SE NÃO CARREGAR)
-    img.onerror = () => {
-      img.src = "https://via.placeholder.com/300x450/111/ff0000?text=Daniel+Canais";
+    // fallback manual
+    poster.onerror = () => {
+      poster.style.backgroundImage =
+        "url(https://via.placeholder.com/300x450/111/ff0000?text=Daniel+Canais)";
     };
 
     const title = document.createElement("h3");
     title.textContent = movie.title;
 
-    card.appendChild(img);
+    card.appendChild(poster);
     card.appendChild(title);
 
     card.onclick = () => {
