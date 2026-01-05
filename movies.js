6d4c8f4c56ca9096056a3fc09a0e6aca9973d307
danielmoraes9776-prog <danielmoraes9776@gmail.com>
2026-01-05 09:50:16 -0300

    Update movies.js

--- movies.js
+++ movies.js
@@ -1,50 +1,9 @@
+alert("movies.js carregou");
+
 const movies = [
   {
-    title: "Night of the Living Dead (1968)",
+    title: "Teste Filme",
     poster: "https://archive.org/services/img/night_of_the_living_dead",
     video: "https://archive.org/embed/night_of_the_living_dead"
-  },
-  {
-    title: "Plan 9 from Outer Space (1959)",
-    poster: "https://archive.org/services/img/Plan9fromOuterSpace",
-    video: "https://archive.org/embed/Plan9fromOuterSpace"
-  },
-  {
-    title: "Charade (1963)",
-    poster: "https://archive.org/services/img/Charade1963",
-    video: "https://archive.org/embed/Charade1963"
-  },
-  {
-    title: "The General (1926)",
-    poster: "https://archive.org/services/img/TheGeneral1926",
-    video: "https://archive.org/embed/TheGeneral1926"
-  },
-  {
-    title: "Metropolis (1927)",
-    poster: "https://archive.org/services/img/metropolis_1927",
-    video: "https://archive.org/embed/metropolis_1927"
-  },
-  {
-    title: "Nosferatu (1922)",
-    poster: "https://archive.org/services/img/nosferatu_1922",
-    video: "https://archive.org/embed/nosferatu_1922"
-  },
-  {
-    title: "Flash Gordon – Ep. 1",
-    poster: "https://archive.org/services/img/flash_gordon_serial",
-    video: "https://archive.org/embed/flash_gordon_serial"
-  },
-  {
-    title: "Popeye Classic",
-    poster: "https://archive.org/services/img/popeye_classic",
-    video: "https://archive.org/embed/popeye_classic"
-  },
-  {
-    title: "Betty Boop Collection",
-    poster: "https://archive.org/services/img/betty_boop",
-    video: "https://archive.org/embed/betty_boop"
-  },
-  {
-    title: "Superman (1940s)",
-    poster: "https://archive.org/services/img/superman_1940s",
-    video:
+  }
+];
